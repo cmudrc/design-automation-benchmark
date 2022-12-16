7c290ef89ca10983e48f0b56aebe4bb0c28d72ae
Chris McComb <ccmcc2012@gmail.com>
2022-12-15 19:17:42 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -3,13 +3,13 @@ import dab.problems
 def solve(problem, algorithm):
     """This does the heavy lifting - its where the problem and the algorithm come together"""  
     import scipy.optimize
-    if algorithm=='SLSQP' or algorithm=='trust-constr':
+    if algorithm.name=='SLSQP' or algorithm.name=='trust-constr':
         return scipy.optimize.minimize(
             problem.objective, # function is the problem's objective function
             problem.initial_solution,  # intial solution is also defined in the problem
-            method=algorithm, # just a string input for right now
+            method=algorithm.name, # just a string input for right now
             bounds=problem.bounds, # get bounds from problem
             constraints=problem.constraints # get constraints from problem
         )
     else:
-        raise ValueError("The input is not one of `SLSQP` and `trust-constr`") # currently only these two solvers work
+        raise ValueError("The algorithm is not one of `SLSQP` and `trust-constr`") # currently only these two solvers work
