a3ea632bb57b2cd1893ad611f696b919d0826bc3
Chris McComb <ccmcc2012@gmail.com>
2022-12-15 19:34:42 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -15,12 +15,3 @@ def solve(problem, algorithm):
     else:
         raise ValueError("The algorithm is not one of `SLSQP` and `trust-constr`") # currently only these two solvers work
 
-def solve_matrix(problems, algorithms):
-    """This does the even heavier lifting - it runs matrices of problems against matrices of algorithms"""
-    results = []
-    for algorithm in algorithms:
-        for problem in problems:
-            result = solve(problem, algorithm)
-            results.append(result)
-    return results
-
