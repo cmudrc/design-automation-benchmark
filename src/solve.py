6f1c764b11b4a33785d305690f72b09a927e7711
Chris McComb <ccmcc2012@gmail.com>
2022-12-15 19:05:46 -0500

    Update solve.py

--- src/solve.py
+++ src/solve.py
@@ -1,14 +1,1 @@
-import scipy.optimize
 
-def solve(problem, algorithm):
-    """This does the heavy lifting - its where the problem and the algorithm come together"""  
-    if algorithm=='SLSQP' or algorithm=='trust-constr':
-        return scipy.optimize.minimize(
-            problem.objective, # function is the problem's objective function
-            problem.initial_solution,  # intial solution is also defined in the problem
-            method=algorithm, # just a string input for right now
-            bounds=problem.bounds, # get bounds from problem
-            constraints=problem.constraints # get constraints from problem
-        )
-    else:
-        raise ValueError("The input is not one of `SLSQP` and `trust-constr`") # currently only these two solvers work
